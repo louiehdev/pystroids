da98aed35020bed4471060b31bfc5222b4ff5d84
chknofthewoods <cotwgames@gmail.com>
2025-05-06 11:06:23 -0600

    B: added player and draw code

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,7 +18,11 @@ def main():
                 return
             
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)
+        dt = dt/1000
+        
 
 if __name__ == "__main__":
     main()
